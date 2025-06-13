698fdcd0c7cbbc691db4ff059f52f2d3587a0af9
subhankar-das-phantom <subhankarnew1@gmail.com>
2025-06-13 20:52:38 +0530

    Update App.jsx

--- TODO/src/App.jsx
+++ TODO/src/App.jsx
@@ -8,10 +8,14 @@ function App() {
     <>
       <Navbar />
       <div className="container bg-blue-100 my-5  mx-auto min-h-[80vh] max-w-[75%]">
-        <div className="">
-          <h1 className="font-bold text-center">Manage Your Task List</h1>
+          <h1 className="font-bold text-center text-3xl ">Manage Your Task List</h1>
+          <div className="addtodo">
+            <h1 className="font-bold mx-9 my-2 text-2xl">Make a todo</h1>
+            <input type="text" className='bg-white mx-5' />
+            <button>Save</button>
+          </div>
         </div>
-      </div>
+
     </>
   )
 }
